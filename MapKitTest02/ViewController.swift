fbf6dcfe84b0281cb4a0ac0e6949a7dea917aefb
D7703_16 <d7703_16@d7703-16ui-imac.local>
2019-09-16 15:02:07 +0900

    mapViewKit 1번째

--- MapKitTest02/ViewController.swift
+++ MapKitTest02/ViewController.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // DIT 위도 경도 설정.
+        let location = CLLocationCoordinate2D(latitude: 35.165005, longitude: 129.071484)
+        
+        //반경 설정
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        
+        //regin 설정
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        //mapView에 add
+        mapView.setRegion(region, animated: true)
     }
-
+    
+    
 
 }
 
